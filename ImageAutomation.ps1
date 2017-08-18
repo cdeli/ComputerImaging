3ad235eafa77c0b2b31c483a4437b307670c84e4
Corey Deli <iwifia@live.com>
2017-08-18 02:15:28 -0400

    Updates to all parameters.

--- ImageAutomation.ps1
+++ ImageAutomation.ps1
@@ -1,8 +1,8 @@
 #########################################
 # Author: Corey Deli                    #
 # Page: https://coreydeli.com           #
-# Date: 4/4/2017                        #
-# Version: 1.1                          #
+# Date: 8/18/2017                       #
+# Version: 1.2                          #
 #########################################
 
 # Parameters
@@ -10,42 +10,46 @@
 Param(
     [parameter(Mandatory=$true)]
     [alias("build")]
-    $customini,
+    $customini, #This is path to the Customini file. This can be removed from the Parameters and set as a variable should you chose to do so
     
     [parameter(Mandatory=$true)] 
-    [alias("deploy")] 
-    $dplpath, 
+    [alias("DeployPath")] 
+    $dplpath, #Path to the Deployment share. Ex. \\ContosoServer\DeploymentShare
+
+    [parameter(Mandatory=$true)]
+    [alias("CapturePath")]
+    $capturepath, #This is the path where the capture folder lives. Ex. \\ContosoServer\TestShare
     
     [parameter(Mandatory=$true)] 
-    [alias("ts")] 
-    $tasks, 
+    [alias("TaskSequence")] 
+    $tasks, #Task sequence
     
     [parameter(Mandatory=$true)] 
-    [alias("hv")] 
-    $hvhost, 
+    [alias("HvHost")] 
+    $hvhost, #Name of the Hyper-V host server. This can as well be hard coded.
     
     [parameter(Mandatory=$true)] 
     [alias("vhd")] 
-    $VHDLocal, 
+    $VHDLocal, #Location of the VHD on the Hyper-V host.
     
     [parameter(Mandatory=$true)] 
     [alias("boot")] 
-    $media, 
+    $media, #Path to your ISO for creating the image.
+
+    [parameter(Mandatory=$true)]
+    [alias("VnetSwitch")]
+    $VnetSwitch,
     
-    #[parameter(Mandatory=$true)] 
-    #[alias("vnic")] 
-    #$vmnic, 
+    [alias("Log")] 
+    $logpath, #Set this path if you want to create a log.
     
-    [alias("l")] 
-    $logpath, 
-    
-    [alias("sendto")] 
+    [alias("SendTo")] 
     $to, 
     
-    [alias("from")] 
+    [alias("SendFrom")] 
     $from, 
     
-    [alias("mail")] 
+    [alias("MailServer")] 
     $server, 
     
     [switch]$compat, 
@@ -55,42 +59,26 @@ Param(
 
 # Variables
 
-#Log File
-
 $logfile = ("image-factory{0:yyyy-MM-dd-HH-mm-ss}.log" -f (Get-Date))
 $log = "$logpath\$logfile"
 
 $subject = "Image Factory Log File"
-
-
 
 ## Start Logging
 if ($logpath) {
     Start-transcript $log
 }
 
-## Location of the "CustomSettings.ini"
-#$customini = "\\Contoso-svr-MDT\TestBuild$\Control\"
-
-## Possible tasks sequences. Use Task ID's
-#$tasks = "REFW10-X64-001"#,"NEXT","NEXT" < ADD ADDITIONAL AND REMOVE POUND SIGN FOR MULTIPLE ID's.
-
-## Hyper-V Settings
-#$hvhost = "Contoso-HV.Contoso.pri"
-#$VHDLocal = "\\Contoso-HV\VmBuild"
-#$VHDRemote = "\\Contoso-HV\VmBuild"
-#$media = "\\Contoso-HV\VmBuild\TestTouch.iso"
-
 ## MDT Locations
-$dplpath = "\\Contoso-svr-MDT\Deployment$"
-$capturepath = "\\Contoso-svr-MDT\TestBuild$\Captures"
+#$dplpath = "\\Contoso-svr-MDT\Deployment$"
+#$capturepath = "\\Contoso-svr-MDT\TestBuild$\Captures"
 
 ## Import Hyper-V 2012r2 Modules. Disable if on 2016
-Import-Module Hyper-V -RequiredVersion 1.1
-#Import-Module Hyper-V -RequiredVersion 2 # This is the module for Hyper-V 2016
+Import-Module -Name Hyper-V -RequiredVersion 1.1
+#Import-Module -Name Hyper-V -RequiredVersion 2 # This is the module for Hyper-V 2016
 Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
 
-# Actual script running
+# Actual Running script
 
 ForEach ($id in $tasks) {
  
@@ -106,7 +94,7 @@ Add-Content $customini\CustomSettings.ini "SkipComputerName=YES"
 ## Build the VM to create the image on.
 $vmname = ("build-{0:yyyy-MM-dd-HH-mm}" -f (get-date))
 New-VM -Name $vmname -MemoryStartupBytes 4096MB -BootDevice CD -Generation 1 -NewVHDPath $VHDLocal\$vmname.vhdx -NewVHDSizeBytes 130048MB `
- -SwitchName "Microsoft Network Adapter Multiplexor Driver - Virtual Switch" -ComputerName $hvhost
+ -SwitchName $VnetSwitch -ComputerName $hvhost
 Set-VM $vmname -ProcessorCount 2 -StaticMemory -ComputerName $hvhost
 Set-VMDvdDrive -VMName $vmname -ControllerNumber 1 -ControllerLocation 0 -Path $media -ComputerName $hvhost
 Start-VM $vmname -ComputerName $hvhost
@@ -128,7 +116,7 @@ Start-Sleep -s 5
 New-PsDrive -Name "DS002" -PSProvider MDTProvider -Root $dplpath -verbos
 
 ## Find the files.
-$wims = Get-ChildItem $capturepath\*.wim
+$wims = Get-ChildItem $capturepath\Captures\*.wim
 
 ## Import the reference images into Production
 ForEach ($file in $wims) {
@@ -146,5 +134,10 @@ if ($logpath) {
 # Email Report
 if ($server) {
     $body = Get-Content -Path $log | out-string 
-    send-mailmessages -to $to -from $from -subject $subject -Body $body -smtpserver $server
+    send-mailmessages `
+        -to $to `
+        -from $from `
+        -subject $subject `
+        -Body $body `
+        -smtpserver $server
 }
